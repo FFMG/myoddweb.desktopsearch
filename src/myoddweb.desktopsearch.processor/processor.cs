805d7f8932d03dd038e5edd40e8b980f9d816a8d
FFMG <github@myoddweb.com>
2018-07-17 21:50:40 +0200

    Added processor project ... to process the files/folders

---
+++ src/myoddweb.desktopsearch.processor/processor.cs
@@ -0,0 +1,37 @@
+﻿//This file is part of Myoddweb.DesktopSearch.
+//
+//    Myoddweb.DesktopSearch is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Myoddweb.DesktopSearch is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Myoddweb.DesktopSearch.  If not, see<https://www.gnu.org/licenses/gpl-3.0.en.html>.
+
+using System.Threading;
+
+namespace myoddweb.desktopsearch.processor
+{
+  public class Processor
+  {
+    /// <summary>
+    /// Start processor.
+    /// </summary>
+    public void Start(CancellationToken token)
+    {
+    }
+
+
+    /// <summary>
+    /// Stop processor
+    /// </summary>
+    public void Stop()
+    {
+    }
+  }
+}
