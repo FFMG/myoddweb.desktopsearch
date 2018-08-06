1025fe90152127d78478db6afb7cbb73ecd72f73
Florent Guelfucci <florent.guelfucci@derivco.co.za>
2018-08-06 07:56:54 +0200

    Fixed a bug where the Webserver config was not present

--- src/myoddweb.desktopsearch.service/Configs/Config.cs
+++ src/myoddweb.desktopsearch.service/Configs/Config.cs
@@ -60,7 +60,7 @@ namespace myoddweb.desktopsearch.service.Configs
         Processors = JsonConvert.DeserializeObject<ConfigProcessor>("{}");
       }
 
-      if (null == Processors)
+      if (null == WebServer)
       {
         // set the default values.
         WebServer = JsonConvert.DeserializeObject<ConfigWebServer>("{}");
