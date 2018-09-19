e1dd8abb2002476f55ab64d60ee86ed1df1ae6b8
FFMG <github@myoddweb.com>
2018-09-19 06:59:35 +0200

    Some code cleanup

--- src/myoddweb.desktopsearch.parser.code/CPP.cs
+++ src/myoddweb.desktopsearch.parser.code/CPP.cs
@@ -40,7 +40,7 @@ namespace myoddweb.desktopsearch.parser.code
     /// </summary>
     private readonly Words _keyWords = new Words( new List<string>
     {
-      "asm","else","new","this","auto","enum","operator","throw","bool","explicit","private",
+      "asm","else", "define", "DEFINE", "new","this","auto","enum","operator","throw","bool","explicit","private",
       "true","break","export","protected","try","case","extern","public","typedef","catch",
       "false","register","typeid","char","float","reinterpret_cast","typename","class","for",
       "return","union","const","friend","short","unsigned","const_cast","goto","signed","using",
