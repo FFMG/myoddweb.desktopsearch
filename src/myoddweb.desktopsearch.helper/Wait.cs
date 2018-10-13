1b385330ccbd8e2f7d86b1a23504effc3a1fbf52
FFMG <github@myoddweb.com>
2018-10-13 15:00:25 +0200

    Changed the way we WaitUntil to use less recources

--- src/myoddweb.desktopsearch.helper/Wait.cs
+++ src/myoddweb.desktopsearch.helper/Wait.cs
@@ -28,6 +28,21 @@ namespace myoddweb.desktopsearch.helper
     /// How many ms we want to sleep in case we only have a single cpu
     /// </summary>
     private const int SingleProcessorSleep = 10;
+
+    /// <summary>
+    /// How many ms we want to slee for multiple processors.
+    /// </summary>
+    private const int MultipleProcessorsSleep = 2;
+
+    /// <summary>
+    /// The maximum amount of time we are prepared to delay.
+    /// </summary>
+    private const int MaxDelayPerThread = 100;
+
+    /// <summary>
+    /// The number of counters currently waiting.
+    /// </summary>
+    private static int _waitersUntilCount;
     #endregion
 
     private Wait()
@@ -121,50 +136,68 @@ namespace myoddweb.desktopsearch.helper
     /// <returns></returns>
     private static async Task UntilAsync(Func<bool> what, int numProcessors, CancellationToken token)
     {
-      var spinWait = numProcessors;
-      var count = 0;
-      while (!what())
+      // increment the number of items.
+      Interlocked.Increment(ref _waitersUntilCount);
+      try
       {
-        token.ThrowIfCancellationRequested();
+        // the more 'waiters' we have, the more starved or resources we will become
+        // but we also don't want to wait forever.
+        // the value of _waitersUntilCount can never be '0' because there is at least one, (us).
+        var delay = _waitersUntilCount * (numProcessors == 1 ? SingleProcessorSleep : MultipleProcessorsSleep);
 
-        if (count > numProcessors && numProcessors > 1 )
-        {
-          // reset
-          count = 0;
+        // make sure that the value is within range.
+        delay = delay > MaxDelayPerThread ? MaxDelayPerThread : delay;
 
-          // The number of iterations we want our spinwait to do.
-          // SpinWait essentially puts the processor into a very tight loop, with the loop count specified by the iterations parameter.
-          // The duration of the wait therefore depends on the speed of the processor.
-          //
-          // do some noop operations on our current CPU
-          // @see https://msdn.microsoft.com/en-us/library/system.threading.thread.spinwait%28v=vs.110%29.aspx?f=255&MSPPError=-2147217396
-          Thread.SpinWait(spinWait);
-          spinWait = spinWait > 1024 ? numProcessors : spinWait*2;
-          continue;
-        }
+        // start number of spin waits.
+        var spinWait = numProcessors;
+        var count = 0;
+        while (!what())
+        {
+          token.ThrowIfCancellationRequested();
 
-        if (count == 0)
-        {
-          // just do a small single yield the first time.
-          // if we only have one processor, and no one else is waiting, this will return straight back.
-          await Task.Yield();
-        }
-        else if( numProcessors == 1 )
-        {
-          // we only have one processor so we don't want to always yield
-          // because if we are the only thread then we will yeald straight back to ourselves
-          // so we don't want the CPU to run hot on our behalf.
-          // so we just sleep a little...
-          Thread.Sleep(SingleProcessorSleep);
-        }
-        else
-        {
-          // cause a small delay that will yield to other CPUs
-          await Task.Delay(count, token );
-        }
+          if (count > numProcessors && numProcessors > 1)
+          {
+            // reset
+            count = 0;
 
-        // move on.
-        ++count;
+            // The number of iterations we want our spinwait to do.
+            // SpinWait essentially puts the processor into a very tight loop, with the loop count specified by the iterations parameter.
+            // The duration of the wait therefore depends on the speed of the processor.
+            //
+            // do some noop operations on our current CPU
+            // @see https://msdn.microsoft.com/en-us/library/system.threading.thread.spinwait%28v=vs.110%29.aspx?f=255&MSPPError=-2147217396
+            Thread.SpinWait(spinWait);
+            spinWait = spinWait > 1024 ? numProcessors : spinWait * _waitersUntilCount;
+            continue;
+          }
+
+          if (count == 0)
+          {
+            // just do a small single yield the first time.
+            // if we only have one processor, and no one else is waiting, this will return straight back.
+            await Task.Yield();
+          }
+          else if (numProcessors == 1)
+          {
+            // we only have one processor so we don't want to always yield
+            // because if we are the only thread then we will yeald straight back to ourselves
+            // so we don't want the CPU to run hot on our behalf.
+            // so we just sleep a little...
+            Thread.Sleep(SingleProcessorSleep);
+          }
+          else
+          {
+            // cause a small delay that will yield to other CPUs
+            await Task.Delay(count * delay, token);
+          }
+
+          // move on.
+          ++count;
+        }
+      }
+      finally
+      {
+        Interlocked.Decrement(ref _waitersUntilCount);
       }
     }
 
@@ -201,4 +234,4 @@ namespace myoddweb.desktopsearch.helper
       UntilAsync(what).Wait();
     }
   }
-  }
+}
