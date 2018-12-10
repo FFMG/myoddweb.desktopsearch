656bb33ce67081439d9e183825b791b8ed5166db
FFMG <github@myoddweb.com>
2018-12-10 21:44:15 +0200

    Created ITransactions so we can get rid of helpers being passed all over the place

---
+++ src/myoddweb.desktopsearch.interfaces/Persisters/ITransaction.cs
@@ -0,0 +1,16 @@
+﻿namespace myoddweb.desktopsearch.interfaces.Persisters
+{
+  public interface ITransaction
+  {
+    /// <summary>
+    /// Prepare for a transaction
+    /// </summary>
+    void Prepare(IPersister persister, IConnectionFactory factory );
+
+    /// <summary>
+    /// Complete a transaction
+    /// </summary>
+    /// <param name="success">If the transaction was successfull or not.</param>
+    void Complete(bool success);
+  }
+}
