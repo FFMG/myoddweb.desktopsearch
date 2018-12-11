231746f3e33508b3fe8623237947b99a61e79c6b
Florent Guelfucci <florent.guelfucci@derivco.co.za>
2018-12-11 17:19:59 +0200

    Started work in AsyncLock

---
+++ src/myoddweb.desktopsearch.helper.test/LockTest.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using myoddweb.desktopsearch.helper.Lock;
+using NUnit.Framework;
+
+namespace myoddweb.desktopsearch.parser.test
+{
+  [TestFixture]
+  internal class LockTest
+  {
+    [Test]
+    public async Task TestSingleEntryAsync()
+    {
+      var gotLock = false;
+      var l = new Lock();
+      using (await l.TryAsync().ConfigureAwait(false))
+      {
+        gotLock = true;
+      }
+      Assert.True(gotLock);
+    }
+
+    [Test]
+    public void TestSingleEntry()
+    {
+      var gotLock = false;
+      var l = new Lock();
+      using (l.Try())
+      {
+        gotLock = true;
+      }
+      Assert.True(gotLock);
+    }
+
+    [Test]
+    public async Task ManualReleaseLockAsync()
+    {
+      var gotLock = false;
+      var wasDisposed = false;
+      var l = new Lock();
+      var key = await l.TryAsync().ConfigureAwait(false);
+      try
+      {
+        gotLock = true;
+      }
+      finally 
+      {
+        key.Dispose();
+        wasDisposed = true;
+      }
+      Assert.True(gotLock);
+      Assert.True(wasDisposed);
+    }
+
+    [Test]
+    public void ManualReleaseLock()
+    {
+      var gotLock = false;
+      var wasDisposed = false;
+      var l = new Lock();
+      var key = l.TryAsync();
+      try
+      {
+        gotLock = true;
+      }
+      finally
+      {
+        key.Dispose();
+        wasDisposed = true;
+      }
+      Assert.True(gotLock);
+      Assert.True(wasDisposed);
+    }
+
+    [Test]
+    public async Task MultipleEntries()
+    {
+      var gotLock = 0;
+      var l = new Lock();
+      using (await l.TryAsync().ConfigureAwait(false))
+      {
+        ++gotLock;
+      }
+      using (await l.TryAsync().ConfigureAwait(false))
+      {
+        ++gotLock;
+      }
+      Assert.AreEqual(2, gotLock);
+    }
+
+    private async Task<bool> LockingTest( Lock l )
+    {
+      using (await l.TryAsync().ConfigureAwait(false))
+      {
+        return true;
+      }
+    }
+
+    private async Task<bool> Blah(Lock l)
+    {
+      var context = Guid.NewGuid().ToString();
+      var valueToDisplay = Thread.CurrentThread.ManagedThreadId;
+TestContext.WriteLine($"{context}: {valueToDisplay}");
+      await Task.Yield();
+      using (await l.TryAsync().ConfigureAwait(false))
+      {
+        await Task.Yield();
+        await Task.Delay(500, CancellationToken.None).ConfigureAwait(false);
+TestContext.WriteLine($"{context}: {valueToDisplay}");
+        if (await LockingTest(l))
+        {
+          await Task.Yield();
+TestContext.WriteLine($"{context}: {valueToDisplay}");
+          return true;
+        }
+      }
+      return false;
+    }
+
+    [Test]
+    public async Task ReEntryTest()
+    {
+      var canceler = new CancellationTokenSource();
+      var gotLock = false;
+      var l = new Lock();
+      var worker1 = Task.Factory.StartNew(async () => await Blah(l), canceler.Token );
+      var worker2 = Task.Factory.StartNew(async () => await Blah(l), canceler.Token);
+      var worker3 = Task.Factory.StartNew(async () => await Blah(l), canceler.Token);
+
+      Task.Delay(2000, CancellationToken.None).Wait(CancellationToken.None);
+      canceler.Cancel();
+      await Task.WhenAll( new []{worker1,worker2, worker3} ).ConfigureAwait(false);
+      Assert.True( gotLock );
+    }
+  }
+}
---
+++ src/myoddweb.desktopsearch.helper/Lock/Lock.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace myoddweb.desktopsearch.helper.Lock
+{
+  internal class Key : IDisposable
+  {
+    #region Member Variables
+    private readonly Lock _parent;
+
+    private readonly long _id = (long) (((ulong) Thread.CurrentThread.ManagedThreadId) << 32) | ((uint) (Task.CurrentId ?? 0));
+    #endregion
+
+    public Key(Lock parent)
+    {
+      _parent = parent;
+    }
+
+    public void Dispose()
+    {
+      _parent.Exit(_id);
+    }
+
+    public async Task<IDisposable> TryAsync()
+    {
+      await _parent.EnterAsync(Task.CurrentId ?? 0).ConfigureAwait(false);
+      return this;
+    }
+  }
+
+  public class Lock
+  {
+    #region Member Variables
+    private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+
+    private readonly object _counterLock = new object();
+
+    private int _counter;
+    #endregion
+
+    public Task<IDisposable> TryAsync()
+    {
+      var key = new Key( this );
+      return key.TryAsync();
+    }
+
+    public IDisposable Try()
+    {
+      var key = new Key(this);
+      return key.TryAsync().GetAwaiter().GetResult();
+    }
+
+    internal async Task EnterAsync(long id)
+    {
+      // get the lock
+      await _semaphore.WaitAsync().ConfigureAwait( false );
+
+      // we are in, so increase the count.
+      lock (_counterLock)
+      {
+        ++_counter;
+      }
+    }
+
+    internal void Exit( long id )
+    {
+      // get the lock and release if need be.
+      lock (_counterLock)
+      {
+        if (_counter == 0)
+        {
+          throw new InvalidOperationException( "Error trying to release the lock already released." );
+        }
+        --_counter;
+
+        // release the lock if we are the last one.
+        if (_counter == 0)
+        {
+          _semaphore.Release();
+        }
+      }
+    }
+  }
+}
