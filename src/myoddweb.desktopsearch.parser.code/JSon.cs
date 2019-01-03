36c46197de1344174448c07a1c80ad37f44d5b42
Florent Guelfucci <florent.guelfucci@derivco.co.za>
2019-01-03 11:50:50 +0200

    Added 'json' file parser

---
+++ src/myoddweb.desktopsearch.parser.code/JSon.cs
@@ -0,0 +1,62 @@
+﻿//This file is part of Myoddweb.DesktopSearch.
+//
+//    Myoddweb.DesktopSearch is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Myoddweb.DesktopSearch is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Myoddweb.DesktopSearch.  If not, see<https://www.gnu.org/licenses/gpl-3.0.en.html>.
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using myoddweb.desktopsearch.helper.Components;
+using myoddweb.desktopsearch.interfaces.IO;
+using myoddweb.desktopsearch.interfaces.Logging;
+
+namespace myoddweb.desktopsearch.parser.code
+{
+  public class Json : IFileParser
+  {
+    public string Name => "JsonParser";
+
+    public string[] Extenstions => new[] {"json" };
+
+    /// <summary>
+    /// The file parser
+    /// </summary>
+    private readonly FileParser _parser;
+
+    public Json()
+    {
+      // @see https://www.regular-expressions.info/unicode.html
+      // But we basically split any words by ...
+      //   \p{Z}\t\r\n\v\f  - All the blank spaces
+      //   \p{P}            - Punctuation
+      //   \p{C}            - Invisible characters.
+      //   \p{S}            - All the symbols, (currency/maths)
+      //
+      // So we allow Numbers and Words together.
+      const int maxNumberReadCharacters = 5000000; // (char = 2bytes * 5000000 = ~10MB)
+      _parser = new FileParser(@"[^\p{Z}\t\r\n\v\f\p{P}\p{C}\p{S}]+", maxNumberReadCharacters);
+    }
+
+    /// <inheritdoc />
+    public bool Supported(FileSystemInfo file)
+    {
+      //  if this is a valid extension ... say yes.
+      return helper.File.IsExtension(file, Extenstions);
+    }
+
+    /// <inheritdoc />
+    public Task<long> ParseAsync(IParserHelper helper, ILogger logger, CancellationToken token)
+    {
+      return _parser.ParserAsync(helper, null, token);
+    }
+  }
+}
